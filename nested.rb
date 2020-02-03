86826974b592e723f1f0b4089161c1f34a38031a
Willis Aybar <willisaybar@gmail.com>
2020-02-03 00:54:19 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 programmer_hash[:grace_hopper]
-# programmer_hash[:alan_kay][:known_for]
+
 
 end
 
@@ -38,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
